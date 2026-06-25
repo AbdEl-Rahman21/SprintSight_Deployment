72328a376164b56f35965e25c53b2dde4cd68672
AbdEl-Rahman21 <abdel292004@gmail.com>
2026-06-26 00:47:46 +0300

    Fix db deployment

---
+++ database/init-scripts/04-restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Restoring PostgreSQL dump..."
+
+pg_restore \
+  --data-only \
+  --no-owner \
+  --no-privileges \
+  --disable-triggers \
+  -U "$POSTGRES_USER" \
+  -d "$POSTGRES_DB" \
+  /docker-entrypoint-initdb.d/03-data.dump
+  
+touch /tmp/restore-complete
+
+echo "Restore completed."
+
